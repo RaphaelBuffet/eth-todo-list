d0fe1a2d3fc4f753e689bcd5ab7fa2f376b8be79
RaphaelBuffet <37210896+RaphaelBuffet@users.noreply.github.com>
2022-05-31 10:54:10 +0200

    stable version

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
 
 contract TodoList {
   uint public taskCount = 0;
@@ -22,7 +23,7 @@ contract TodoList {
     bool completed
   );
 
-  constructor() public {
+  constructor() {
     createTask("Check out dappuniversity.com");
   }
 
